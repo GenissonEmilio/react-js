a0389278fb44ba9f44f21d557c4f9b22982e92e0
Genisson Emilio <esgenisson@gmail.com>
2025-03-26 15:24:11 -0300

    Update Form.js

--- react-project/src/components/Form.js
+++ react-project/src/components/Form.js
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault();
+        console.log(name);
         console.log("Usuario cadastrado");
     }
+
+    const [ name, setName ] = useState();
 
     return (
         <div>
             <h1>Meu formulario</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" id="name" name="name" placeholder="Digite o seu nome" />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" placeholder="Digite sua senha" id="password" name="password" 
+                    onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
